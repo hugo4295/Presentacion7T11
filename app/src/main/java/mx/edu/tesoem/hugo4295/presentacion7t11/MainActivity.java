8bee526da423af92060f7d38a59f55f5a90ad51f
Tesoem <Tesoem@ISC-Tesoem-iMac.local>
2019-03-26 13:14:47 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/hugo4295/presentacion7t11/MainActivity.java
@@ -0,0 +1,23 @@
+package mx.edu.tesoem.hugo4295.presentacion7t11;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Pantalla2Activity.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
+    }
+}
---
+++ app/src/main/java/mx/edu/tesoem/hugo4295/presentacion7t11/Pantalla2Activity.java
@@ -0,0 +1,13 @@
+package mx.edu.tesoem.hugo4295.presentacion7t11;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pantalla2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla2);
+    }
+}
